1be3525ee50a222103f96166766f67536016f54d
josephsmendoza <josephsmendoza@gmail.com>
2022-12-01 05:57:16 +0000

    add --random-timeout

--- CSSRandom.js
+++ CSSRandom.js
@@ -1,4 +1,10 @@
-(function CSSRandom(){
-    document.body.style.setProperty('--random',Math.random())
-    requestAnimationFrame(CSSRandom)
-})()
+const style=document.body.style
+function CSSRandom(){
+    style.setProperty('--random',Math.random())
+    const timeout=style.getPropertyValue('--random-timeout')
+    if(timeout){
+        setTimeout(CSSRandom,parseInt(timeout))
+    }else{
+        requestAnimationFrame(CSSRandom)
+    }
+}
